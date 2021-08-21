8de6de251608cebacd5c342b1fe4d02dd196604e
kotobukid <kotobuki@daqua.jp>
2021-08-21 16:05:17 +0900

    スクロールの抑制

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+document.body.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+}, { passive: false });
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
